d1bac06579ed3d5b18fe7c21c1d776e7482c2d70
Toni <toni.dezman@gmail.com>
2022-09-06 17:32:25 +0200

    solution for parity coding challenge

--- epi_judge_python/parity.py
+++ epi_judge_python/parity.py
@@ -2,8 +2,12 @@ from test_framework import generic_test
 
 
 def parity(x: int) -> int:
-    # TODO - you fill in here.
-    return 0
+    res = 0
+    while x:
+        if x & 1:
+            res ^= 1
+        x >>= 1
+    return res
 
 
 if __name__ == '__main__':
