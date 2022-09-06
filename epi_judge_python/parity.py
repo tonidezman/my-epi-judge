5e5825288666f0d5c7b5c4829369ad01042ac43f
Toni <toni.dezman@gmail.com>
2022-09-06 17:41:49 +0200

    faster solution for parity

--- epi_judge_python/parity.py
+++ epi_judge_python/parity.py
@@ -4,9 +4,8 @@ from test_framework import generic_test
 def parity(x: int) -> int:
     res = 0
     while x:
-        if x & 1:
-            res ^= 1
-        x >>= 1
+        res ^= 1
+        x &= (x - 1)
     return res
 
 
