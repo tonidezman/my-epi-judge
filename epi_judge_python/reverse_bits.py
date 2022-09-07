0ca281973e7330bcf86ddccc283d30dc4e15f08f
Toni <toni.dezman@gmail.com>
2022-09-07 19:55:34 +0200

    small improvement

--- epi_judge_python/reverse_bits.py
+++ epi_judge_python/reverse_bits.py
@@ -2,10 +2,11 @@ from test_framework import generic_test
 
 def reverse_bits(x: int) -> int:
     res = 0
-    for i in range(32):
+    num_bits = 64
+    for i in range(num_bits // 2):
         right_bit = (x >> i) & 1
-        left_bit = (x >> (63 - i)) & 1
-        res |= right_bit << (63 - i)
+        left_bit = (x >> (num_bits - 1 - i)) & 1
+        res |= right_bit << (num_bits - 1 - i)
         res |= left_bit << i
     return res
 
