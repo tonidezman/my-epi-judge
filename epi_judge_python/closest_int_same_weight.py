659d6a005242dace3405f659d784ed874fc1338e
Toni <toni.dezman@gmail.com>
2022-09-13 18:49:46 +0200

    evan faster solution

--- epi_judge_python/closest_int_same_weight.py
+++ epi_judge_python/closest_int_same_weight.py
@@ -1,23 +1,17 @@
 from test_framework import generic_test
 
-def get_weight(x):
-    res = 0
-    while x:
-        x = x & (x - 1)
-        res += 1
-    return res
+def swap_bits(x, i, j):
+    mask = (1 << i) | (1 << j)
+    return x ^ mask
 
 def closest_int_same_bit_count(x: int) -> int:
-    weight_x = get_weight(x)
-    curr_neg = x - 1
-    curr_pos = x + 1
+    i, j = 0, 1
     while True:
-        if get_weight(curr_neg) == weight_x:
-            return curr_neg
-        if get_weight(curr_pos) == weight_x:
-            return curr_pos
-        curr_neg -= 1
-        curr_pos += 1
+        if ((x >> i) & 1) ^ ((x >> j) & 1):
+            return swap_bits(x, i, j)
+        i += 1
+        j += 1
+
 
 
 
