f82827df7fcbdec0799833e03c91c8825aa1e394
Toni <toni.dezman@gmail.com>
2022-09-14 13:12:28 +0200

    solution to reverse digits

--- epi_judge_python/reverse_digits.py
+++ epi_judge_python/reverse_digits.py
@@ -2,8 +2,19 @@ from test_framework import generic_test
 
 
 def reverse(x: int) -> int:
-    # TODO - you fill in here.
-    return 0
+    # handle negative ints
+    sign = 1
+    if x < 0:
+        sign = -1
+        x = abs(x)
+    # reverse digits
+    res = 0
+    while x:
+        last_digit = x % 10
+        res *= 10
+        res += last_digit
+        x //= 10
+    return sign * res
 
 
 if __name__ == '__main__':
